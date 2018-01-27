10d92774e9b9e5a0e91e78ee849ec2882ba8dfc5
Vivek Tumrukota <vivek@workspan.com>
2018-01-26 22:17:01 -0800

    Add package.lock

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+export const NAVBAR = {
+  SOCIAL_MEDIA: [
+    {
+      name: 'Linkedin',
+      link: 'https://www.linkedin.com/in/vtumrukota/',
+      icon: '/assets/images/sf-bg.jpg',
+    },
+    {
+      name: 'Angel List',
+      link: 'https://angel.co/vivekt',
+      icon: '/assets/images/sf-bg.jpg',
+    },
+    {
+      name: 'Github',
+      link: 'https://github.com/vtumrukota',
+      icon: '/assets/images/sf-bg.jpg',
+    }
+  ]
+};
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+export class NavbarComponent implements OnInit {
+  socialMedia = NAVBAR.SOCIAL_MEDIA;
+
+  constructor() {
+    'ngInject';
+  }
 
   ngOnInit() {
+    console.log('test', this.socialMedia);
   }
 
 }
