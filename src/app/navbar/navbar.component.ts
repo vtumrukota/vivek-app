37003b97a22ca67d9f5b184ae63bbb5f3aabc689
Vivek Tumrukota <vivek@workspan.com>
2018-02-02 18:22:08 -0800

    udgrade and add icons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,17 +5,17 @@ export const NAVBAR = {
     {
       name: 'Linkedin',
       link: 'https://www.linkedin.com/in/vtumrukota/',
-      icon: '/assets/images/sf-bg.jpg',
-    },
-    {
-      name: 'Angel List',
-      link: 'https://angel.co/vivekt',
-      icon: '/assets/images/sf-bg.jpg',
+      icon: '/assets/images/linkedin-logo.png',
     },
     {
       name: 'Github',
       link: 'https://github.com/vtumrukota',
-      icon: '/assets/images/sf-bg.jpg',
+      icon: '/assets/images/GitHub-logo.png',
+    },
+    {
+      name: 'Angel List',
+      link: 'https://angel.co/vivekt',
+      icon: '/assets/images/angellist-icon.svg',
     }
   ]
 };
